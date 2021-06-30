80ed2dac77180bea64c6e9bf3a992327f8107be5
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-06-30 23:20:27 +0700

    first commit

---
+++ src/main/java/com/example/sudungspringsecurity/SudungSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sudungspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudungSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudungSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sudungspringsecurity/SudungSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sudungspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudungSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
